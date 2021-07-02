1949ad7930e8a374bd5697a2615589b5fc7e6cd9
Yugal Agarwal <yugal1434@gmail.com>
2021-07-02 20:09:14 +0530

    second commit

---
+++ auto.py
@@ -0,0 +1,19 @@
+print("Time: ")
+
+t = int(input())
+
+if t == 8:
+    print("Brush your teeths.")
+elif t == 9:
+    print("Have some breakfast!!")
+elif t == 14:
+    print("Have lunch.")
+elif t == 16:
+    print("Go play football :) ")
+elif t == 20:
+    print("Have dinner")
+elif t == 22:
+    print("spend some time with family")
+elif t == 23:
+    print("Have a nice sleep!!")
+    print("Sweet dreams :)")
